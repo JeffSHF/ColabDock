a31186925566fe47eb8e26f1c47b2846b823a3b3
JeffSHF <jeffrey.f1993@gmail.com>
2023-07-14 02:39:09 -0400

    fix bugs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='docking with experimental restraints',
     long_description="Inverting AlphaFold2 structure prediction model for protein-protein docking with experimental restraints",
     long_description_content_type='text/markdown',
-    packages=find_packages(include=['colabdock', 'colabdesign']),
+    packages=['colabdock', 'colabdesign'],
     install_requires=['py3Dmol','absl-py','biopython',
                       'chex','dm-haiku','dm-tree',
                       'immutabledict','jax','ml-collections',
